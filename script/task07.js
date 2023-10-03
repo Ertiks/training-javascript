c3b060ee2ed516769af37c2d1b88b5ff864a7419
Ertiks <ertiks42@yahoo.fr>
2023-10-03 22:03:31 +0200

    task 7

--- script/task07.js
+++ script/task07.js
@@ -1,8 +1,58 @@
-const link = document.querySelector('footer a');
+let link = document.querySelector('footer a');
+const footer = document.querySelector('footer');
+const div = document.querySelector('footer > div')
+function extractValue() {
 
-console.log(link);
+    l1 = document.cookie.split("=");
+    return l1[1];
 
-document.cookie = 'name=DefaultCookie; value=true';
-alert(document.cookie);
+}
+
+function activebox(activeBool) {
+
+    if (activeBool) {
+        div2.hidden = false;
+        div.hidden = true;
+    }
+    else {
+        div2.hidden = true;
+        div.hidden = false;
+    }
+
+}
 
 
+isSecondBoxActive = false;
+
+//Calcul 1j de cookie
+const d = new Date();
+d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
+
+//whitebox N2
+let div2 = document.createElement('div');
+footer.appendChild(div2);
+div2.innerHTML = '<button type="button">Delete Cookies</button>';
+button = div2.querySelector('button');
+div2.hidden = true;
+
+//whitebox N1
+div.innerHTML;
+
+
+link.addEventListener('click', function () {
+
+    document.cookie = 'Simple-cookie = true; expires=' + d.toUTCString() + '; path=/;';
+    if (extractValue() == "true") {
+        activebox(true);
+    }
+
+})
+
+button.addEventListener('click', function () {
+
+
+    document.cookie = "Simple-cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    activebox(false);
+
+
+})
